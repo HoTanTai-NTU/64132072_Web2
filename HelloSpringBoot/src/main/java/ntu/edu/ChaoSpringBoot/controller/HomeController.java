04f03bf45ed69cc8ab9dffa9d0243d83233405f7
HoTanTai-NTU <tai.ht.64cntt@ntu.edu.vn>
2025-03-10 22:20:12 +0700

    Spring Boot Commit

---
+++ HelloSpringBoot/src/main/java/ntu/edu/ChaoSpringBoot/controller/HomeController.java
@@ -0,0 +1,12 @@
+package ntu.edu.ChaoSpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String trangChu() {
+		return "index";
+	}
+}
