38e0bdfffa5992c182c1751060ac746d3b18f096
HoTanTai-NTU <tai.ht.64cntt@ntu.edu.vn>
2025-04-01 09:38:51 +0700

    Push lần 4

--- HoTanTai_BaiThiGK/src/main/java/thiGK/ntu64132072/HoTanTai_BaiThiGK/controller/WebController.java
+++ HoTanTai_BaiThiGK/src/main/java/thiGK/ntu64132072/HoTanTai_BaiThiGK/controller/WebController.java
@@ -1,7 +1,13 @@
 package thiGK.ntu64132072.HoTanTai_BaiThiGK.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class WebController {
@@ -25,4 +31,17 @@ public class WebController {
 	public String xoaTopic() {
 		return "deletetopic";
 	}
+	@RequestMapping("/topic")
+	public class PageController {
+
+	    @GetMapping("/all")
+	    public String listPages(ModelMap model) {
+	        model.addAttribute("topics", topics);
+	        return "list";
+	    }
+
+	    @GetMapping("/new")
+	    public String addNewTopicForm(ModelMap model) {
+	        return "addnew";
+	    }
 }
